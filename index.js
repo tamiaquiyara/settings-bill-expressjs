6d819cca3d347e92968b0ff6da273b92ea3c050b
Tamia Quiyara <mia.mbuckingworth@gmail.com>
2022-08-16 11:13:16 +0200

    Added routes and rendered data

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
-const exphbs  = require('express-handlebars');
 let express = require('express');
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const SettingsBill = require('./settings-bill');
+
 const app = express();
+const settingsBill = SettingsBill();
 
-// app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs.engine({defaultLayout: false}));
+app.set('view engine', 'handlebars');
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get("/", function(req, res){
-  res.send("Bill With Settings App");
+  res.render("index", {
+    settings: settingsBill.getSettings(),
+    totals: settingsBill.totals()
+  });
 });
 
-app.post('/settings ', function(req, res){
+app.post('/settings', function(req, res){
+    
+
+    settingsBill.setSettings({
+      callCost: req.body.callCost,
+      smsCost: req.body.smsCost,
+      warningLevel: req.body.warningLevel,
+      criticalLevel: req.body.criticalLevel
+    });
+    
+    console.log(settingsBill.getSettings());
+  
+   res.redirect('/');
+});
+
+app.post('/action', function(req, res){
+
+    //capture the call type to add
+    // console.log(req.body.actionType);
+
+    settingsBill.recordAction(req.body.actionType);
+
+   res.redirect('/');
+});
+
+app.get('/actions', function(req, res){
 
 });
 
-app.post('/action ', function(req, res){
-
-});
-
-app.get('/actions ', function(req, res){
-
-});
-
-app.get('/actions/:type ', function(req, res){
+app.get('/actions/:type', function(req, res){
 
 });
 
